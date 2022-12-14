8ac959020642f59275212b5b67dd10908e95ff2b
Jordan Sampson <jordansampson23@gmail.com>
2022-12-14 01:28:42 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const allClearBtn = document.querySelector('[data-all-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const equalsBtn = document.querySelector('[data-equals]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
